fc01856b1433a10a387e6a237b6e160c66cf4cc3
EvgeniyKostin <kostinen@icloud.com>
2021-10-08 16:06:25 +0300

    Initial Commit

---
+++ swifUIandURLSession/swifUIandURLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifUIandURLSessionApp.swift
+//  swifUIandURLSession
+//
+//  Created by Evgeniy Kostin on 08.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct swifUIandURLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
